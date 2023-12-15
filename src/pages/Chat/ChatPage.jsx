07b85e41080bdd3ff03c0860bf6909ced0cbfc33
chanhee won <wch2208@gmail.com>
2023-12-15 17:41:13 +0900

    PC버전 레이아웃 수정, 인풋값참조방법수정

--- src/pages/Chat/ChatPage.jsx
+++ src/pages/Chat/ChatPage.jsx
@@ -9,20 +9,58 @@ import {
   Button,
   Stack,
 } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import styled from "@emotion/styled";
 import SendIcon from "@mui/icons-material/Send";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import OpenAI from "openai";
 import { useState, useEffect, useRef } from "react";
 
 function ChatPage() {
+  // ------------------   makeStyles -----------------//
+
+  const theme = createTheme();
+  const StyledContainer = styled(Grid)(({ theme }) => ({
+    backgroundColor: "#bacee0",
+    height: "100vh",
+    width: "100vw",
+    maxWidth: "1024px",
+    margin: "0, auto",
+    paddingTop: "10px",
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    // 미디어 쿼리를 사용하여 브레이크포인트에 따른 스타일 정의
+    [theme.breakpoints.up("md")]: {
+      border: "4px solid black",
+      borderRadius: "30px",
+      height: "84vh",
+      width: "900px",
+      margin: "auto",
+      marginTop: "5%",
+    },
+  }));
+  const StyledChat = styled(Grid)(({ theme }) => ({
+    maxHeight: "calc(84vh - 64px)",
+    minHeight: "calc(84vh - 64px)",
+    overflowY: "auto",
+    mb: 0.5,
+    // 미디어 쿼리를 사용하여 브레이크포인트에 따른 스타일 정의
+    [theme.breakpoints.up("md")]: {
+      maxHeight: "calc(64vh - 64px)",
+      minHeight: "calc(64vh - 64px)",
+    },
+  }));
+
+  // ------------------   makeStyles -----------------//
+
   const apiKey = import.meta.env.VITE_API_KEY;
   const openai = new OpenAI({ apiKey: apiKey, dangerouslyAllowBrowser: true });
 
-  let [inputMessage, setInputMessage] = useState("");
   let [messageList, setMessageList] = useState([]);
   let [threadId, setThreadId] = useState("");
   let [loading, setLoading] = useState(false);
   const chatScrollRef = useRef(null);
+  const inputRef = useRef("");
 
   //스레드를 생성하고 대화 시작을 위한 인사를 먼저 세팅한다.
   useEffect(() => {
@@ -47,7 +85,6 @@ function ChatPage() {
 
   //------------  채팅 시작 버튼  ---------------//
   async function runStart() {
-    console.log("시작 버튼 감지");
     setLoading(true);
 
     //메세지 생성
@@ -66,7 +103,6 @@ function ChatPage() {
       run = await openai.beta.threads.runs.retrieve(threadId, run.id);
       //반복간격 0.5초
       await new Promise(resolve => setTimeout(resolve, 500));
-      console.log("run 감지중");
     }
 
     //실행 완료 후 메시지 출력
@@ -75,19 +111,18 @@ function ChatPage() {
     setMessageList([...messageList, messages.data[1], messages.data[0]]);
 
     setLoading(false);
-
-    console.log("messageList:", messageList);
   }
 
   //-----------------   전송 버튼   ---------- //
-  async function sendMessage(inputMessage) {
-    console.log("전송 버튼 감지");
+
+  async function sendMessage() {
+    const message = inputRef.current.value;
     //로딩 시작
     setLoading(true);
     //인풋창의 텍스트를 넣어서 메세지 생성
     await openai.beta.threads.messages.create(threadId, {
       role: "user",
-      content: inputMessage,
+      content: message,
     });
 
     //런 실행
@@ -100,7 +135,6 @@ function ChatPage() {
       run = await openai.beta.threads.runs.retrieve(threadId, run.id);
       //반복간격 0.5초
       await new Promise(resolve => setTimeout(resolve, 500));
-      console.log("run 감지중");
     }
 
     //실행 완료 후 메시지 출력
@@ -109,13 +143,12 @@ function ChatPage() {
 
     //메시지 리스트에 추가
     setMessageList([...messageList, messages.data[1], messages.data[0]]);
-    setInputMessage("");
+    //setInputMessage("");
     setLoading(false);
   }
 
   // -------------------    종료 버튼 ---------------//
   async function endOfInterview() {
-    console.log("종료 버튼 감지");
     setLoading(true);
     //메세지 생성
     await openai.beta.threads.messages.create(threadId, {
@@ -141,132 +174,112 @@ function ChatPage() {
 
     //메시지 리스트에 추가
     setMessageList([...messageList, messages.data[1], messages.data[0]]);
-    setInputMessage("");
     setLoading(false);
   }
 
   return (
-    <Grid
-      container
-      justifyContent="center"
-      alignItems="center"
-      sx={{
-        bgcolor: "#bacee0",
-        height: "100vh",
-        width: "100vw",
-        maxWidth: "1024px",
-        m: "0 auto",
-        pt: 1,
-        pl: 2,
-        pr: 2,
-      }}
-    >
-      <Grid
-        item
-        xs={12}
-        sx={{
-          textAlign: "center",
-          mb: 0.5,
-        }}
-      >
-        <Paper
-          elevation={6}
+    <ThemeProvider theme={theme}>
+      <StyledContainer container justifyContent="center" alignItems="center">
+        <Grid
+          item
+          xs={12}
           sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            p: "2px 4px",
+            textAlign: "center",
+            mb: 0.5,
           }}
         >
-          <ListItemText primary="시작을 누르고 인터뷰를 진행하세요." />
-          <ListItemText primary="완료를 누르면 기사를 작성합니다." />
-        </Paper>
-      </Grid>
-      <Grid
-        id="chatScroll"
-        item
-        xs={12}
-        ref={chatScrollRef}
-        sx={{
-          maxHeight: "calc(84vh - 64px)",
-          minHeight: "calc(84vh - 64px)",
-          overflowY: "auto",
-          mb: 0.5,
-        }}
-      >
-        <List>
-          {messageList.map((data, index) =>
-            data.role === "assistant" ? (
-              <ListItem key={index}>
-                <Paper elevation={1} sx={{ p: 1, maxWidth: "70%" }}>
-                  <ListItemText primary={data.content[0].text.value} />
-                </Paper>
-              </ListItem>
-            ) : (
-              <ListItem key={index} sx={{ justifyContent: "flex-end" }}>
-                <Paper
-                  elevation={1}
-                  sx={{
-                    p: 1,
-                    maxWidth: "70%",
-                    bgcolor: "#FEE500",
-                  }}
-                >
-                  <ListItemText primary={data.content[0].text.value} />
-                </Paper>
-              </ListItem>
-            )
-          )}
-        </List>
-      </Grid>
-      <Grid item xs={12} sx={{ mb: 0.5 }}>
-        <Stack spacing={2} direction="row" sx={{ justifyContent: "center" }}>
-          <Button variant="contained" size="small" onClick={runStart}>
-            시작
-          </Button>
-
-          <Button variant="contained" size="small" onClick={endOfInterview}>
-            종료
-          </Button>
-        </Stack>
-      </Grid>
-
-      <Grid item xs={12}>
-        <Paper
-          variant="outlined"
-          sx={{ display: "flex", alignItems: "center", p: "2px 4px", mb: 2 }}
-        >
-          <InputBase
-            fullWidth
-            placeholder="메시지를 입력하세요"
-            value={inputMessage}
-            disabled={loading}
-            sx={{ ml: 1, flex: 1 }}
-            onChange={e => {
-              setInputMessage(e.target.value);
+          <Paper
+            elevation={6}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              p: "2px 4px",
             }}
-            onKeyDown={e => {
-              if (e.key === "Enter") {
-                sendMessage(inputMessage);
-              }
+          >
+            <ListItemText primary="시작을 누르고 인터뷰를 진행하세요." />
+            <ListItemText primary="완료를 누르면 기사를 작성합니다." />
+          </Paper>
+        </Grid>
+        <StyledChat id="chatScroll" item xs={12} ref={chatScrollRef}>
+          <List>
+            {messageList.map((data, index) =>
+              data.role === "assistant" ? (
+                <ListItem key={index}>
+                  <Paper elevation={1} sx={{ p: 1, maxWidth: "80%" }}>
+                    <ListItemText primary={data.content[0].text.value} />
+                  </Paper>
+                </ListItem>
+              ) : (
+                <ListItem key={index} sx={{ justifyContent: "flex-end" }}>
+                  <Paper
+                    elevation={1}
+                    sx={{
+                      p: 1,
+                      maxWidth: "80%",
+                      bgcolor: "#FEE500",
+                      wordBreak: "break-word",
+                    }}
+                  >
+                    <ListItemText primary={data.content[0].text.value} />
+                  </Paper>
+                </ListItem>
+              )
+            )}
+          </List>
+        </StyledChat>
+        <Grid item xs={12} sx={{ mb: 0.5 }}>
+          <Stack spacing={2} direction="row" sx={{ justifyContent: "center" }}>
+            <Button variant="contained" size="small" onClick={runStart}>
+              시작
+            </Button>
+
+            <Button variant="contained" size="small" onClick={endOfInterview}>
+              종료
+            </Button>
+          </Stack>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Paper
+            variant="outlined"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              p: "2px 4px",
+              mb: 2,
+              bgcolor: loading ? "gray" : false,
             }}
-          />
-          {loading ? (
-            <AccessTimeIcon />
-          ) : (
-            <IconButton
-              color="primary"
-              sx={{ p: "10px" }}
-              onClick={() => {
-                sendMessage(inputMessage);
+          >
+            <InputBase
+              fullWidth
+              placeholder="메시지를 입력하세요"
+              disabled={loading}
+              sx={{ ml: 1, flex: 1 }}
+              inputRef={inputRef}
+              onKeyDown={e => {
+                if (e.key === "Enter") {
+                  sendMessage(inputRef.current.value);
+                }
               }}
-            >
-              <SendIcon />
-            </IconButton>
-          )}
-        </Paper>
-      </Grid>
-    </Grid>
+            />
+            {loading ? (
+              <AccessTimeIcon />
+            ) : (
+              <IconButton
+                color="primary"
+                sx={{ p: "10px" }}
+                onClick={() => {
+                  sendMessage(inputRef.current.value);
+                }}
+              >
+                <SendIcon />
+              </IconButton>
+            )}
+          </Paper>
+        </Grid>
+      </StyledContainer>
+    </ThemeProvider>
   );
 }
 export default ChatPage;
