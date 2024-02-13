efd87926e68328d686490701a59e9fc725078a27
chanhee won <wch2208@gmail.com>
2024-02-14 00:02:36 +0900

    HomePage MUI styled 적용하여 인라인 sx 속성코드 분리하기

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,24 +1,15 @@
-import { Grid } from "@mui/material";
 import BasicSpeedDial from "../../components/common/SpeedDial";
 import ListSection from "./components/ListSection";
 import HeroSection from "./components/HeroSection";
+import OutlineContainer from "./components/ui/OutlineContainer";
 
 function HomePage() {
   return (
-    <Grid
-      id="outline"
-      container
-      sx={{
-        overflow: "hidden",
-        backgroundColor: "#f2f2f2",
-        p: 0,
-        m: 0,
-      }}
-    >
+    <OutlineContainer>
       <HeroSection />
       <ListSection />
       <BasicSpeedDial />
-    </Grid>
+    </OutlineContainer>
   );
 }
 export default HomePage;
---
+++ src/pages/Home/components/ui/OutlineContainer.js
@@ -0,0 +1,10 @@
+// 파일 경로: src/components/ui/OutlineContainer.js
+
+import { Grid, styled } from "@mui/material";
+
+const OutlineContainer = styled(Grid)({
+  overflow: "hidden",
+  backgroundColor: "#f2f2f2",
+});
+
+export default OutlineContainer;
